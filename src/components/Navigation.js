b96a6c674d7777f91c8ca05c75a34a2eb5908b72
dblinkhorn <dblinkhorn@gmail.com>
2022-01-13 23:38:33 -0800

    fixed background color overlay not stretching to fill height & width. added quicksand font. changed main logo.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,35 +1,37 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './Navigation.css'
-import Favicon from '../static/images/favicon.png'
+import Logo from '../static/images/logo.png'
 
 const Navigation = () => {
   return (
     <div className='navigation'>
       <div className='logo-left'>
-        <img src={Favicon} alt='logo'></img>
+        <Link to='/'>
+          <img src={Logo} alt='Logo' />
+        </Link>
       </div>
       <nav>
         <ul>
-          <li className='menu-button'>
-            <Link to="/">Home</Link>
+          <li className='nav-button'>
+            <Link to='/'>HOME</Link>
           </li>
-          <li className='menu-button'>
-            <Link to="/mission">Our Mission</Link>
+          <li className='nav-button'>
+            <Link to='/mission'>OUR MISSION</Link>
           </li>
-          <li className='menu-button'>
-            <Link to="/about">About Us</Link>
+          <li className='nav-button'>
+            <Link to='/about'>ABOUT US</Link>
           </li>
-          <li className='menu-button'>
-            <Link to="/offerings">Offerings</Link>
+          <li className='nav-button'>
+            <Link to='/offerings'>OFFERINGS</Link>
           </li>
-          <li className='menu-button'>
-            <Link to="/samples">Sample Lessons</Link>
+          <li className='nav-button'>
+            <Link to='/samples'>SAMPLE LESSONS</Link>
           </li>
         </ul>
       </nav>
-      <div className='contact-button-div'>
-        Contact Us
+      <div className='contact-us'>
+        CONTACT US
       </div>
     </div>
   )
