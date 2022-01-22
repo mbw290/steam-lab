9c0452150f65aa1c31f16f9ab3f7fe1230f15ad4
dblinkhorn <dblinkhorn@gmail.com>
2022-01-22 01:17:44 -0800

    continued work on mobile styling and responsiveness. also added overview info card to bottom of page. removed intro video from top of page

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
       </div>
       <Outlet />
     </div>
-  );
+  )
 }
 
 export default App;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,21 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import kidsHome from '../../static/images/kids-home.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCut, faVideo, faFileAlt } from '@fortawesome/free-solid-svg-icons'
 import './Home.css'
 import '../../App.css'
+
+const iconStyling = {
+  padding: '16px'
+}
 
 const Home = () => {
   return (
     <>
-      <div className='intro-video__container'>
+      {/* <div className='intro-video__container'>
         <video playsInline controls autoPlay muted style={{opacity: 1, maxWidth: 1200, height: 500}}>
           <source src="https://mrreedssteamlab.s3-us-west-1.amazonaws.com/Steam+Lab+Intro.mp4" type="video/mp4" />
         </video>
-      </div>
+      </div> */}
       <div className='home-container'>
         <div className='home-text'>
           <p className='motto'>Where Excitement<br />and Learning<br /><span className='blue-text'>Come Together</span></p>
-          <div className='description'>ONLINE STEAM VIDEO CHALLENGES TO SUPPORT STUDENTS, TEACHERS AND EXPANDED LEARNING PROGRAMS</div>
+          <div className='description'>Online STEAM video challenges to support students, teachers, and expanded learning programs</div>
           <div className='buttons__container'>
             <div className='contact-button__container'>
               <Link to='/contact'>
@@ -36,13 +42,22 @@ const Home = () => {
       <div className='offerings__container'>
         <div className='offerings'>
           <div className='offerings-left'>
-            Left
+            <div className='video-icon__container'>
+              <FontAwesomeIcon icon={faVideo} color='white' size='2x' style={iconStyling} />
+            </div>
+            <span>42 Challenge Videos (6 per grade-level, K-6)</span>
           </div>
           <div className='offerings-center'>
-            Center
+            <div className='video-icon__container'>
+              <FontAwesomeIcon icon={faFileAlt} color='white' size='2x' style={iconStyling} />
+            </div>
+            <span>Additional teacher resource documents for extension and enrichment</span>
           </div>
           <div className='offerings-right'>
-            Right
+            <div className='video-icon__container'>
+              <FontAwesomeIcon icon={faCut} color='white' size='2x' style={iconStyling} />
+            </div>
+            <span>Designed for easy implementation with easy-to-find materials</span>
           </div>
         </div>
       </div>
