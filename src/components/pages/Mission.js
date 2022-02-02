2a28492a245d2d7ddf66d63214fa029f0798bdfd
dblinkhorn <dblinkhorn@gmail.com>
2022-02-02 00:08:04 -0800

    added english learners section to our mission page. still have some formatting/styling left

--- src/components/pages/Mission.js
+++ src/components/pages/Mission.js
@@ -60,6 +60,59 @@ const Mission = () => {
               flexibility in that they may be used to address both philosophies.</p>
             </div>
           </div>
+          <div className='video__container'>
+            <video playsInline controls style={{opacity: 1, maxWidth: 1200, maxHeight: 500}}>
+              <source src="https://mrreedssteamlab.s3-us-west-1.amazonaws.com/Our+Philosophy.mp4" type="video/mp4" />
+            </video>
+          </div>
+        </div>
+      </div>
+      <div className='mission__image-container'>
+        <div className='mission__image-our-mission'>
+          <img src={ RocketLogo } alt="Rocket logo" />
+        </div>
+        <div className='mission__text'>
+          <div className='mission__heading-container'>
+            <span className='mission__heading'>English Learners</span>
+          </div>
+          <p className='mission__body-text'>
+            <div>
+              <p className='mission__section-heading'>Transforming Classrooms, Schools, and Lives</p>
+              <p>English Language learners consist of a diverse group of students who typically lack access to Science,
+              Technology, Engineering, and Math (STEM) opportunities. A new report by the National Academies of Sciences
+              and Engineering states that a shift is needed in how STEM is taught to English Learners.</p>
+
+              <p>Often these students are denied access to STEM learning opportunities. The report states that language and
+              content are learned concurrently and not separately or consecutively. As such, English Learners need STEM
+              experiences early in their education when their peers are gaining exposure to STEM content. The suggestion is
+              that curriculum developers should work to develop materials and resources that target English Language Learners.
+              With appropriate curriculum, these students can participate, contribute and succeed in STEM classrooms.</p>
+
+              <p>Education Week reported in 2018 that school systems across the country should do more to ensure that current
+              and former English-language learners have access to STEM education. “Too often schools operate under the incorrect
+              assumption that proficiency in English is a prerequisite to meaningful engagement with STEM learning and fail to
+              leverage ELs meaningful engagement with content and disciplinary practices as a route to language proficiency,”
+              the report authors wrote. The study also found that, “Recognition of the assets that ELs bring to the classroom,
+              and that some deficits in student performance arise from lack of access rather than limited ability or language
+              proficiency, or from cultural differences.</p>
+
+              <p>EdSurge research found that obstacles to English learners participating in STEM subjects and careers do not
+              include lack of interest or ability, according to the report, “English Learners in STEM Subjects: Transforming
+              Classrooms, Schools and Lives.” Rather, it stems from a lack of access to rigorous, age-appropriate instruction
+              in the field. The problem can be attributed to a variety of reasons; one is that many schools assume English
+              learners cannot participate in rigorous STEM content learning before reaching a certain level of English
+              proficiency—an assumption that research has disproven, according to the authors.</p>
+
+              <p>Mr Reed's Steam Lab video challenges are designed to give an immersive experience that EL students can access,
+              enjoy, and in which they can experience success.  Each activity is designed with a specific grade level in mind,
+              however teachers may use their discretion when it comes to assigning or using the videos. Many of the literacy
+              resources may be adjusted for different levels of reading ability.</p>
+
+              <cite className='citation'>National Academies of Sciences, Engineering, and Medicine. 2018. English Learners in STEM Subjects:
+              Transforming Classrooms, Schools, and Lives. Washington, DC: The National Academies Press.
+              <br />Source: <a href='https://doi.org/10.17226/25182'>https://doi.org/10.17226/25182</a>.</cite>
+            </div>
+          </p>
         </div>
       </div>
     </div>
